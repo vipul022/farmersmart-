56763e61fae71686f566db4d8e515d3141386651
vipul022 <vipul.seth022@gmail.com>
2020-07-26 16:31:15 +1000

    edited update and destroy methods in listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,9 @@
 class ListingsController < ApplicationController
-  #This will let all the users to view index and show page only
+  #This will allow users to view index and show page only if they are not signed in
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
+      #show all listings
       @listings = Listing.all
   
   end
@@ -10,11 +11,11 @@ class ListingsController < ApplicationController
   def create
       #create a listing for current user
         @listing = current_user.listings.create(listing_params)
-        
+
         if @listing.errors.any?
             render "new"
         else
-            redirect_to listings_path
+            redirect_to @listing
         end
   end
 
@@ -29,9 +30,15 @@ class ListingsController < ApplicationController
   end
 
   def update
+      #update the current listing
+      @listing.update(listing_params)
+      redirect_to @listing
   end
 
   def destroy
+      #deleted current listing
+      @listing.destroy
+      redirect_to listings_path
   end
 
   private
