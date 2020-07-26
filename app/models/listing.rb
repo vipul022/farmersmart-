824b7226d2d8baec1db2bccd5d310fa743ddef2a
vipul022 <vipul.seth022@gmail.com>
2020-07-26 16:07:59 +1000

    added validations for input fields

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,6 @@ class Listing < ApplicationRecord
   belongs_to :category
   belongs_to :munit
   belongs_to :user
+  #validations 
+  validates :title, :price, :quantity, :munit, presence: true
 end
