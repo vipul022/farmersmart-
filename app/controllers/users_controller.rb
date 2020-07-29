7a047f01cd0699b0957d633af6a2698041ebab2a
vipul022 <vipul.seth022@gmail.com>
2020-07-29 11:15:40 +1000

    added logic to display user's name,bio and all the listings of a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
   def show
   #search for a particular user in database 
       @user = User.find(params[:id])
+  #search all the listings for a particular user to display is user's show page    
+      @user_listings = @user.listings
+      p "********"
+      p @user_listings
   end
 end
+
