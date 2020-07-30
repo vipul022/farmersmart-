03a0b0557d5c5939de270f51964ec074390cf69b
vipul022 <vipul.seth022@gmail.com>
2020-07-30 16:46:52 +1000

    added code comments

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,7 @@ class Listing < ApplicationRecord
   validates :title, :price, :quantity, :munit, presence: true
   #This will allow user to attach a photo to each listing
   has_one_attached :photo
-  
+  #set up many to many relation between listing and cart through join table
   has_many :cart_listings
   has_many :carts, through: :cart_listings
 
