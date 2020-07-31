a8221cfc9e4b9431922660a34fa843b356388fe8
vipul022 <vipul.seth022@gmail.com>
2020-07-31 19:50:17 +1000

    fixed add to cart link on listings show page and added logic to  create method in carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,12 +3,30 @@ class CartsController < ApplicationController
   def index
     if user_signed_in? && current_user.cart
       @cart_listings = current_user.cart.listings
+      p "@@@@@@@"
+      pp @cart_listings
+      
     else
       redirect_to listings_path
     end
   end
-
+# if user's cart is empty, a cart can be created for the user and the completed status is set to false,in case user is already having a cart and when user click on add to cart link on the listing, the listing can be added to the cart
   def create
+      if !current_user.cart
+        cart = Cart.create(completed: false, user_id: current_user.id)
+      else
+        cart = current_user.cart
+    # p "/////////"
+      # p cart.listings
+       
+      end
+    # p "/////////listing"
+      # p params[:listing_id]
+      listing = Listing.find(params[:listing_id])
+      cart.listings << listing
+      # p "@@@@@@@"
+      # pp listing
+      redirect_to carts_path
   end
 
 end
