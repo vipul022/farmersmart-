189323f4b3ca3c63731860e96436f72d208980e2
vipul022 <vipul.seth022@gmail.com>
2020-07-31 12:11:44 +1000

    added logic in index action in carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,14 @@
 class CartsController < ApplicationController
+#this action will store all the listings that have been added to cart by current user in a variable that will be used in html view.  
+  def index
+    if user_signed_in? && current_user.cart
+      @cart_listings = current_user.cart.listings
+    else
+      redirect_to listings_path
+    end
+  end
+
   def create
   end
 
-  def index
-  end
 end
