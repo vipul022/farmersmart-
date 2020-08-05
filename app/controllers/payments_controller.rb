df129d198dcf79868498cd4ef476865b92220c30
vipul022 <vipul.seth022@gmail.com>
2020-08-05 11:24:58 +1000

    created payments controller and added basic method for creating a new stripe session

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,24 @@
+
+def get_stripe_id
+  @listing = Listing.find(params[:id])
+  session_id = Stripe::Checkout::Session.create(
+    payment_method_types: ['card'],
+    customer_email: current_user.email,
+    line_items: [{
+      name: @listing.title,
+      description: @listing.description,
+      amount: @listing.deposit * 100,
+      currency: 'aud',
+      quantity: 1,
+    }],
+    payment_intent_data: {
+      metadata: {
+        user_id: current_user.id,
+        listing_id: @listing.id
+      }
+    },
+    success_url: "#{root_url}payments/success?userId=#{current_user.id}&listingId=#{@listing.id}",
+    cancel_url: "#{root_url}listings"
+  ).id
+  render :json => {id: session_id, stripe_public_key: Rails.application.credentials.dig(:stripe, :public_key)}
+end
