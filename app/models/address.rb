0241fe9b7b2bd71ab5bce6a8186e901353cb51d0
vipul022 <vipul.seth022@gmail.com>
2020-08-11 11:58:39 +1000

    added code comments and added .DS_store file to .gitignore file

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
+#Address model relation with user
   belongs_to :user
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
+#Category model relation with listings
   has_many :listings
 end
